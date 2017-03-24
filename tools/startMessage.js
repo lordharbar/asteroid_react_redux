e026fedb8a5b9a0d485142aead2f29269a0d89ee
Sam Biggs <lordharbar@users.noreply.github.com>
2017-03-23 21:59:43 -0600

    update message

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -2,4 +2,4 @@ import colors from 'colors';
 
 /*eslint-disable no-console */
 
-console.log("winter is coming, aplication starting in dev mode...".green);
+console.log("winter is coming, application starting in dev mode...".green);
